dbff520ac454d17b9da132360bca4e3745fd342a
NVRC <nathaniel.charlebois@gmail.com>
2017-02-02 15:23:14 -0500

    Improving server

--- src/server/Server.java
+++ src/server/Server.java
@@ -9,7 +9,13 @@
 *
 *Purpose:           Manage the communication between two RPi and relay instructions
 *
-*Update Log:		v1.0.1
+*Update Log:
+*					v1.0.2
+*						- add general program flow logic
+*						- randButton
+*						- some code clean up and commenting
+
+*					v1.0.1
 *						- UI hooked in
 *						- some clean up + commenting
 *						- socket init now done internally by constructor
@@ -27,6 +33,7 @@ package server;
 //import external libraries
 import java.net.*;
 import java.util.Scanner;
+import java.util.Random;
 
 //import packages
 import ui.SimpleUI;
@@ -45,6 +52,8 @@ public class Server
 	private DatagramPacket sendPacket;
 	private RPi rpi1;
 	private RPi rpi2;
+	private Random rand;
+	private byte[] firstButton;
 
 
 	//generic constructor
@@ -91,18 +100,7 @@ public class Server
     private RPi initConnection()
     {
        receivePacket();
-       byte[] data = receivePacket.getData();
-
-       //check if data is handshake
-       if(data.equals(HANDSHAKE))
-       {
-    	   return new RPi(receivePacket.getAddress(), receivePacket.getPort());
-       }
-       else
-       {
-    	   ui.println("Connection failed: Received packet did not complete handshake");
-    	   return null;
-       }
+       return new RPi(receivePacket.getAddress(), receivePacket.getPort());
     }
 
    private void sendPacket(byte[] data, RPi pi)
@@ -121,9 +119,9 @@ public class Server
    private void receivePacket(){
       try
       {
-            ui.println("Waiting for packet on port: " + generalSocket.getPort()) ;
-            generalSocket.receive(receivePacket) ;
-            ui.println( receivePacket.getAddress() + " " + receivePacket.getPort() + ": " + new String(receivePacket.getData()).trim() ) ;
+    		ui.println("Waiting for packet on port: " + generalSocket.getLocalPort()) ;
+        	generalSocket.receive(receivePacket) ;
+			ui.println( receivePacket.getAddress() + " " + receivePacket.getPort() + ": " + new String(receivePacket.getData()).trim() ) ;
 
       }
       catch( Exception e )
@@ -142,11 +140,48 @@ public class Server
         }
    }
 
+   //General flow of the game logic
+   public void run(){
+	   //Select first button to be pressed
+	   //Smelly cast
+	   firstButton[0] = (byte)randButton(0,2);
+	   System.out.println("The randomButton is: " + firstButton);
+	   sendPacket(firstButton, rpi1);
+
+	   throughput();
+
+
+   }
+
+   //Passes the data from one RPi to the other
+   private void throughput(){
+	   receivePacket();
+	   int source = 1;
+	   while(receivePacket.getData()[0]!='!'){
+		  if(source == 1){
+			  sendPacket(receivePacket.getData(), rpi2);
+			  source = 2;
+		  }else{
+			  sendPacket(receivePacket.getData(), rpi1);
+			  source = 1;
+		  }
+	   }
+	   return;
+
+   }
+
+   //Returns a value between min-max inclusive
+   private int randButton(int min, int max){
+	   return rand.nextInt((max - min) + 1) + min;
+   }
+
 
    //run an instance of server
    public static void main(String[] args)
    {
        //Init server
        Server server = new Server();
+
+	   server.run();
    }
 }
